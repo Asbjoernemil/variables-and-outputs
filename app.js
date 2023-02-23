a95d8bc2c4b0b7babd65d9187af75a57fb62ddd2
Asbjoernemil <asbjoernemil24@gmail.com>
2023-02-23 13:33:33 +0100

    if else

--- app.js
+++ app.js
@@ -24,5 +24,16 @@ function displayNumber() {
   document.querySelector("#number").textContent = number;
 
   let message = `Tallet er nu: ${number}`;
+
+  if (number > 10) {
+    console.log("tallet er større end 10");
+    message = "Tallet er større end 10";
+  } else if (number == 10) {
+    console.log("tallet er 10");
+    message = "Taller er 10";
+  } else {
+    console.log("tallet er mindre end");
+    message = "tallet er under 10";
+  }
   document.querySelector("#message").textContent = message;
 }
